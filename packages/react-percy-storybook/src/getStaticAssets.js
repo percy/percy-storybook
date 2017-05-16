17608fac9df60967e88a152b04dcc754b02c5698
Tim Haines <tmhaines@gmail.com>
2017-05-16 14:36:42 -0700

    Storybook: More specifically target preview.js when getting assets (#21)

--- packages/react-percy-storybook/src/getStaticAssets.js
+++ packages/react-percy-storybook/src/getStaticAssets.js
@@ -6,7 +6,7 @@ export default function getStaticAssets() {
     const storybookStaticPath = path.resolve('storybook-static');
     const storyHtml = fs.readFileSync(path.join(storybookStaticPath, 'iframe.html'), 'utf8');
 
-    const storybookJavascriptPath = storyHtml.match(/<script src="(.*?)"><\/script>/)[1];
+    const storybookJavascriptPath = storyHtml.match(/<script src="(.*?static\/preview.*?)"><\/script>/)[1];
     const storyJavascript = fs.readFileSync(path.join(storybookStaticPath, storybookJavascriptPath), 'utf8');
 
     const assets = {};
