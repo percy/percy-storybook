234cb0769c52be6900e1943cb27712584c3b1bb7
David Jones <dgjones@gmail.com>
2017-07-05 14:54:48 -0700

    Expand support for fetching Storybook version (#37)
    * the environment var names for storybook change depending on if you installed via yarn or npm and if you have storybook in your devDependencies or dependencies
    
    * small wording change

--- packages/react-percy-storybook/src/storybookVersion.js
+++ packages/react-percy-storybook/src/storybookVersion.js
@@ -1,5 +1,18 @@
 export default function storybookVersion() {
+         // storybook react dependencies
   return process.env['npm_package_dependencies_@storybook/react'] ||
+         process.env.npm_package_dependencies__storybook_react ||
+
+         // storybook react dev dependencies
+         process.env['npm_package_devDependencies_@storybook/react'] ||
+         process.env.npm_package_devDependencies__storybook_react ||
+
+         // kadira storybook dependencies
          process.env['npm_package_dependencies_@kadira/storybook'] ||
+         process.env.npm_package_dependencies__kadira_storybook ||
+
+         // kadira storybook dev dependencies
+         process.env['npm_package_devDependencies_@kadira/storybook'] ||
+         process.env.npm_package_devDependencies__kadira_storybook ||
          'unknown';
 }
