facb249bb85f32fbe32631b183af5f6be87f98b9
Tim Haines <tmhaines@gmail.com>
2017-08-28 22:09:36 -0700

    Rename integration-tests/react-percy-storybook to integration-tests/storybook-for-react

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -16,20 +16,20 @@ link () {
   yarn link @percy-io/$1
 }
 
-if [ "$SUITE" = "react-percy-storybook" ]; then
+if [ "$SUITE" = "storybook-for-react" ]; then
   # If Percy is enabled, and there's a PERCY_TOKEN supplied (it's not on community PRs),
-  # take snapshots of the react-percy-storybook integration tests's stories.
+  # take snapshots of the storybook-for-react integration tests's stories.
   if [[ "$PERCY_ENABLE" != "0" && -n "$PERCY_TOKEN" ]] ; then
-    cd integration-tests/react-percy-storybook
+    cd integration-tests/storybook-for-react
     link percy-storybook
     yarn storybook:percy
   elif [[ "$PERCY_ENABLE" != "0" && "$TRAVIS" != true ]] ; then
-    # This is local, when invoking yarn test:integration react-percy-storybook w/o PERCY_TOKEN
+    # This is local, when invoking yarn test:integration storybook-for-react w/o PERCY_TOKEN
     error "No PERCY_TOKEN given"
   fi
 else
   cat <<EOF
 Valid targets are:
-* react-percy-storybook
+* storybook-for-react
 EOF
 fi
