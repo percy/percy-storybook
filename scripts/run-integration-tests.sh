510b81f2415656c89b7dc8fbcccb517528e1af32
Tim Haines <tmhaines@gmail.com>
2017-08-28 23:22:22 -0700

    Fix a couple of comments

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -29,13 +29,13 @@ if [ "$SUITE" = "storybook-for-react" ]; then
   fi
 elif [ "$SUITE" = "storybook-for-vue" ]; then
   # If Percy is enabled, and there's a PERCY_TOKEN supplied (it's not on community PRs),
-  # take snapshots of the storybook-for-react integration tests's stories.
+  # take snapshots of the storybook-for-vue integration tests's stories.
   if [[ "$PERCY_ENABLE" != "0" && -n "$PERCY_TOKEN" ]] ; then
     cd integration-tests/storybook-for-vue
     link percy-storybook
     yarn storybook:percy
   elif [[ "$PERCY_ENABLE" != "0" && "$TRAVIS" != true ]] ; then
-    # This is local, when invoking yarn test:integration storybook-for-react w/o PERCY_TOKEN
+    # This is local, when invoking yarn test:integration storybook-for-vue w/o PERCY_TOKEN
     error "No PERCY_TOKEN given"
   fi
 else
