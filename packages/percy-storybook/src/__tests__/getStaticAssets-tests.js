67433602d2182c81907c7ed35e26e3a41260b5a6
Tim Haines <tmhaines@gmail.com>
2017-10-06 09:21:44 -0700

    Fix lint

--- packages/percy-storybook/src/__tests__/getStaticAssets-tests.js
+++ packages/percy-storybook/src/__tests__/getStaticAssets-tests.js
@@ -1,13 +1,15 @@
 import { getStorybookJavascriptPath } from '../getStaticAssets';
 
 it('parses and returns path', () => {
-  const html = '<html><head></head><body>' +
+  const html =
+    '<html><head></head><body>' +
     '<script src="static/preview.ca96e23c5dfccc147bb6.bundle.js"></script></body></html>';
   expect(getStorybookJavascriptPath(html)).toEqual('static/preview.ca96e23c5dfccc147bb6.bundle.js');
 });
 
 it('parses and returns path when type present', () => {
-  const html = '<html><head></head><body>' +
+  const html =
+    '<html><head></head><body>' +
     '<script type="text/javascript" src="static/preview.ca96e23c5dfccc147bb8.bundle.js"></script>' +
     '</body></html>';
   expect(getStorybookJavascriptPath(html)).toEqual('static/preview.ca96e23c5dfccc147bb8.bundle.js');
