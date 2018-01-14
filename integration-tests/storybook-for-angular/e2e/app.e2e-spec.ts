e1e42e57e88acbd43aeee62f0905eee06286226e
Tim Haines <tmhaines@gmail.com>
2018-01-13 16:27:43 -0800

    Add Storybook for Angular integration tests

---
+++ integration-tests/storybook-for-angular/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('storybook-for-angular App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
