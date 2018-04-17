1f1dee221e193786de826e2c5a699f889d8edf1c
Tim Haines <tmhaines@gmail.com>
2018-04-16 18:18:10 -0700

    new blue sentence

--- integration-tests/storybook-for-react/stories/index.js
+++ integration-tests/storybook-for-react/stories/index.js
@@ -14,7 +14,9 @@ storiesOf('Static CSS', module)
     <p className="green">Hi there! This text should be green.</p>
   ))
   .add('blue text using static css in sub_dir', () => (
-    <p className="blue">Hi there! This text should be blue when rendered in Percy.</p>
+    <p className="blue">
+      Hi there! This text should be blue when rendered in Percy. New blue sentence.
+    </p>
   ));
 
 storiesOf('Managing Dynamic Data.Frozen Time', module).add('Show the current date', () => (
