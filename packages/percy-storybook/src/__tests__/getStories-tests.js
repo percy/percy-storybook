a5603b69cfae7ac1efa5ba5a836f76e20527109d
Tim Haines <tmhaines@gmail.com>
2018-05-31 17:54:25 -0700

    Allow more time for timing out test to timeout

--- packages/percy-storybook/src/__tests__/getStories-tests.js
+++ packages/percy-storybook/src/__tests__/getStories-tests.js
@@ -5,7 +5,7 @@ let originalTimeout = 0;
 
 beforeEach(function() {
   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 });
 
 afterEach(function() {
