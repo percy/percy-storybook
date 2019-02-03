d8ad7efb4eb2119a18a7e652a9e857567c857394
Tim Haines <tmhaines@gmail.com>
2019-02-02 19:55:36 -0800

    Temp commit to see error message in windows (Appveyor)

--- packages/percy-storybook/src/__tests__/getStories-tests.js
+++ packages/percy-storybook/src/__tests__/getStories-tests.js
@@ -18,6 +18,7 @@ it('raises an error when no stories loaded', async () => {
     const message =
       'Evaluation failed: Error: Stories not found on window within 10 seconds. ' +
       "Check your call to serializeStories in your Storybook's config.js.";
+    expect(e.message).toEqual('');
     expect(e.message.startsWith(message)).toEqual(true);
   }
 
