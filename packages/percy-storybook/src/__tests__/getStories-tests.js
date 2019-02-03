cbc35f4c8328cea62964b84ec2701be757b49f7a
Tim Haines <tmhaines@gmail.com>
2019-02-02 20:30:08 -0800

    Update misc packages (#58)
    * Upgrade misc packages
    
    * Adjust precommit as recommended
    
    * Update Storybook version in integration tests
    
    * Reset yarn.lock
    
    * Lock terser package version to avoid broken version
    
    * Temp commit to see error message in windows (Appveyor)
    
    * Fix test by adding iframe-without-stories html page

--- packages/percy-storybook/src/__tests__/getStories-tests.js
+++ packages/percy-storybook/src/__tests__/getStories-tests.js
@@ -13,7 +13,7 @@ afterEach(function() {
 
 it('raises an error when no stories loaded', async () => {
   try {
-    await getStories({ iframePath: '.' });
+    await getStories({ iframePath: __dirname + '/iframe-without-stories.html' });
   } catch (e) {
     const message =
       'Evaluation failed: Error: Stories not found on window within 10 seconds. ' +
