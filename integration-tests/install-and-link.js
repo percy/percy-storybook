8bcb688b49649bdb5ff7eed99b3fba6b09463133
Robert DeLuca <robertdeluca19@gmail.com>
2020-02-26 16:46:34 -0600

    Smaller node setup script for integration tests

--- integration-tests/install-and-link.js
+++ integration-tests/install-and-link.js
@@ -1,47 +1,12 @@
-let { promisify } = require('util');
-let ogExec = require('child_process').exec;
+let { execSync } = require('child_process');
 let { readdirSync } = require('fs');
 
-let getDirectories = (source = './') =>
-  readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
+readdirSync('./', { withFileTypes: true }).forEach(dirent => {
+  if (dirent.isDirectory()) {
+    let options = { cwd: dirent.name, stdio: 'inherit' };
 
-let dirs = getDirectories();
-
-function exec(cmd, options) {
-  return new Promise((resolve, reject) => {
-    ogExec(cmd, options, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve({ stdout, stderr });
-    });
-  });
-}
-
-async function installAndLink(testProjectDir) {
-  function output({ stderr, stdout }) {
-    console.log(stdout);
-
-    if (stderr) {
-      console.log(stderr);
-    }
+    console.log(`\n--- ${options.cwd} ---\n`);
+    execSync('yarn', options);
+    execSync('yarn link @percy/storybook', options);
   }
-
-  let options = {
-    cwd: testProjectDir
-  };
-
-  console.log('---');
-  output(await exec('yarn', options));
-  console.log('Linking `@percy/storybook`');
-  output(await exec('yarn link @percy/storybook', options));
-}
-
-(async () => {
-  for (testProj of dirs) {
-    await installAndLink(`./${testProj}`);
-  }
-})();
+});
