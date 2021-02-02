0c2f6969262960906d9197d07fc0c424dca0db5f
Wil Wilsman <wil@wilwilsman.com>
2021-02-01 22:14:48 -0600

    ♻ Reorganize command

---
+++ src/commands/storybook/index.js
@@ -0,0 +1,50 @@
+import { existsSync } from 'fs';
+import logger from '@percy/logger';
+import Command from '../../command';
+
+export class Storybook extends Command {
+  static description = 'Snapshot static or hosted Storybook stories';
+
+  static args = [{
+    name: 'url_or_build_dir',
+    description: 'storybook url or build output directory',
+    required: true
+  }];
+
+  static examples = [
+    '$ percy storybook ./build',
+    '$ percy storybook http://localhost:9000/'
+  ];
+
+  log = logger('cli:storybook');
+
+  // Called on error, interupt, or after running
+  async finally() {
+    await super.finally();
+    this.server?.close();
+  }
+
+  // Serves a static directory and resolves when listening.
+  async storybook() {
+    let urlOrDir = this.args['url-or-build-dir'];
+    if (/^https?:\/\//.test(urlOrDir)) return urlOrDir;
+
+    if (!existsSync(urlOrDir)) {
+      return this.error(`Not found: ${urlOrDir}`);
+    }
+
+    this.log.debug(`Serving Storybook build: ${urlOrDir}`);
+
+    let http = require('http');
+    let serve = require('serve-handler');
+
+    return new Promise(resolve => {
+      this.server = http.createServer((req, res) => {
+        serve(req, res, { public: urlOrDir });
+      }).listen(() => {
+        let { port } = this.server.address();
+        resolve(`http://localhost:${port}`);
+      });
+    });
+  }
+}
