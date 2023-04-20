2f325aa85e7e2ea2a2728803254148e790102f12
Jigar Wala <jigarwala007@gmail.com>
2023-04-21 00:42:07 +0530

    remove it from here since its defined in snapshotSchema

--- src/config.js
+++ src/config.js
@@ -42,10 +42,6 @@ export const storybookSchema = {
         queryParams: {
           type: 'object',
           normalize: false
-        },
-        domTransformation: {
-          type: 'string',
-          normalize: false
         }
       }
     },
