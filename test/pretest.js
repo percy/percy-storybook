c252c8738661258343d7b46c3fde66b13365bc2d
nilshah98 <neelshah.1998@gmail.com>
2023-10-25 11:30:09 +0530

    fix: build storybook command in pretest

--- test/pretest.js
+++ test/pretest.js
@@ -2,10 +2,7 @@ import spawn from 'cross-spawn';
 
 let storybookVersion = process.env.STORYBOOK_VERSION || '6';
 let args = storybookVersion === '7' ? ['build'] : [];
-args = args.concat(['--config-dir=./test/.storybook', '--output-dir=./test/.storybook-build', '--loglevel error']);
+args = args.concat(['--config-dir=./test/.storybook', '--output-dir=./test/.storybook-build', '--loglevel=error']);
 let storybookBinary = storybookVersion === '7' ? 'storybook' : 'build-storybook';
-
-console.log(`storybookBinary - ${storybookBinary}`);
-console.log(`storybookArgs - ${args}`);
 
 spawn(storybookBinary, args, { stdio: 'inherit' });
