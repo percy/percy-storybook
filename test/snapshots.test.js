01878e17502b24cf2d68cb246f485cf2475c34bf
Rahul Barnwal <rahul.bar@browserstack.com>
2025-07-30 15:18:06 +0530

    unit tests fixes

--- test/snapshots.test.js
+++ test/snapshots.test.js
@@ -7,40 +7,56 @@ describe('captureDOM', () => {
     page = { eval: jasmine.createSpy('eval'), snapshot: jasmine.createSpy('snapshot') };
     percy = {
       config: { snapshot: { enableJavaScript: false, widths: [800], responsiveSnapshotCapture: false } },
-      client: { getDeviceDetails: jasmine.createSpy('getDeviceDetails').and.returnValue(Promise.resolve([{ width: 375 }, { width: 800 }])) },
+      client: { getDeviceDetails: jasmine.createSpy('getDeviceDetails').and.returnValue(Promise.resolve([])) },
       build: { id: 'buildid' }
     };
     log = { debug: jasmine.createSpy('debug'), warn: jasmine.createSpy('warn'), error: jasmine.createSpy('error') };
     previewResource = { content: '<html>preview</html>' };
   });
 
-  it('returns previewResource.content in dryRun', async function() {
+  it('returns previewResource.content in dryRun (non-responsive)', async function() {
     let flags = { dryRun: true };
     let options = { name: 'story', widths: [800], responsiveSnapshotCapture: false };
+    percy.config.snapshot.responsiveSnapshotCapture = false;
     let gen = captureDOM(page, { id: 'id' }, options, flags, false, previewResource, percy, log);
     let result = await gen.next();
     expect(result.value).toBe('<html>preview</html>');
   });
 
-  it('returns previewResource.content if enableJavaScript', async function() {
+  it('returns previewResource.content if enableJavaScript (non-responsive)', async function() {
     let flags = { dryRun: false };
     let options = { name: 'story', widths: [800], responsiveSnapshotCapture: false };
+    percy.config.snapshot.responsiveSnapshotCapture = false;
     let gen = captureDOM(page, { id: 'id' }, options, flags, true, previewResource, percy, log);
     let result = await gen.next();
     expect(result.value).toBe('<html>preview</html>');
   });
 
-  it('calls page.eval and page.snapshot for non-dryRun', async function() {
+  it('calls page.eval and page.snapshot for non-dryRun (non-responsive)', async function() {
     page.eval.and.returnValue(Promise.resolve());
     page.snapshot.and.returnValue(Promise.resolve({ domSnapshot: '<html>real</html>' }));
     let flags = { dryRun: false };
     let options = { name: 'story', widths: [800], responsiveSnapshotCapture: false };
+    percy.config.snapshot.responsiveSnapshotCapture = false;
     let gen = captureDOM(page, { id: 'id' }, options, flags, false, previewResource, percy, log);
     await gen.next(); // run to yield page.eval
     let result = await gen.next(); // run to yield page.snapshot
     expect(page.eval).toHaveBeenCalled();
     expect(page.snapshot).toHaveBeenCalled();
-    expect(result.value).toBe('<html>real</html>');
+    expect(result.value).toEqual({ domSnapshot: '<html>real</html>' });
+  });
+
+  it('returns array of doms in dryRun (responsive)', async function() {
+    // Responsive mode
+    percy.config.snapshot.responsiveSnapshotCapture = true;
+    let flags = { dryRun: true };
+    let options = { name: 'story', widths: [375, 800], responsiveSnapshotCapture: true };
+    let gen = captureDOM(page, { id: 'id' }, options, flags, false, previewResource, percy, log);
+    let result = await gen.next();
+    expect(Array.isArray(result.value)).toBe(true);
+    expect(result.value.length).toBe(2);
+    expect(result.value[0].width).toBe(375);
+    expect(result.value[1].width).toBe(800);
   });
 });
 
